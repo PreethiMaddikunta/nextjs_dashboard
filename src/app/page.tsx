5f75af26d794cc886a2cf4b3367f3239ff44862c
Preethi Maddikunta <preethisri26@gmail.com>
2023-09-14 16:54:20 +0530

    styles setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 
 export default function Home() {
   return (
+    <div id="dashboard">
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
           <code className="font-mono font-bold">Preeti Sharma</code>
     </main>
+    </div>
   )
 }
